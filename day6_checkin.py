4141f14b4228e64c7dfe224317d7b23e86a77935
atharvp4098 <atharvp4098@gmail.com>
2026-02-25 22:32:51 +0530

    Create day6_checkin.py

---
+++ day6_checkin.py
@@ -0,0 +1,10 @@
+# Day Streak Program
+
+day = int(input("Enter day number: "))
+
+if day % 2 == 0:
+    print("Even Day - Keep Going 💪")
+else:
+    print("Odd Day - Stay Consistent 🔥")
+
+print("No Zero Days 🚀")
